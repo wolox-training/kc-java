8e792875333ced2270e0b95787d21e99519b8595
Kevin Chilito <kevin.chilito@icloud.com>
2021-06-11 11:05:20 -0500

    commit create book repository

---
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -0,0 +1,12 @@
+package wolox.training.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import wolox.training.models.Book;
+
+public interface BookRepository extends JpaRepository<Book, Integer> {
+
+    @Query("SELECT b FROM Book b WHERE LOWER(b.author) = LOWER(:author)")
+    Book bookByAuthor(@Param("author") String author);
+}
