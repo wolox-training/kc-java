ed97de883f0bbf00b924e94d32b4cd5b192a792c
Kevin Chilito <kevin.chilito@icloud.com>
2021-06-15 11:09:20 -0500

    add comments to all methods controller

--- src/main/java/wolox/training/controllers/BookController.java
+++ src/main/java/wolox/training/controllers/BookController.java
@@ -15,28 +15,56 @@ public class BookController {
     @Autowired
     private BookRepository bookRepository;
 
+    /**
+     * This method return all books registers
+     *
+     * @return all books
+     */
     @GetMapping
     public Iterable findAll() {
         return bookRepository.findAll();
     }
 
+    /**
+     * This method return one {@link Book} for author name
+     *
+     * @param authorTitle: author name
+     * @return {@link Book} by author
+     */
     @GetMapping("/author/{author}")
     public Book findByAuthor(@PathVariable String authorTitle) {
         return bookRepository.findByAuthor(authorTitle);
     }
 
+    /**
+     * This method return one {@link Book} for id
+     *
+     * @param id: record in database
+     * @return {@link Book} by id
+     */
     @GetMapping("/{id}")
     public Book findOne(@PathVariable Integer id) {
         return bookRepository.findById(id)
                 .orElseThrow(() -> new BookNotFoundException(id));
     }
 
+    /**
+     * This method creates an {@link Book} with following parameters
+     *
+     * @param book: book for create
+     * @return book by body book
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Book create(@RequestBody Book book) {
         return bookRepository.save(book);
     }
 
+    /**
+     * This method deletes an book for id record
+     *
+     * @param id: book id by delete
+     */
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Integer id) {
         bookRepository.findById(id)
@@ -44,6 +72,13 @@ public class BookController {
         bookRepository.deleteById(id);
     }
 
+    /**
+     * This method replaces an {@link Book} with following parameters
+     *
+     * @param book: book to save
+     * @param id: id to search
+     * @return book update
+     */
     @PutMapping("/{id}")
     public Book updateBook(@RequestBody Book book, @PathVariable Integer id) {
         if (book.getId() != id) {
