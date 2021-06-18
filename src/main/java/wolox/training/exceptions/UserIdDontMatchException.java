335da478e13c2ff939f0f9e5baaed8b0d04fdd22
Kevin Chilito <kevin.chilito@icloud.com>
2021-06-17 20:29:20 -0500

    change http status, NOT ACCEPTABLE by BAD REQUEST

--- src/main/java/wolox/training/exceptions/UserIdDontMatchException.java
+++ src/main/java/wolox/training/exceptions/UserIdDontMatchException.java
@@ -3,6 +3,6 @@ package wolox.training.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_ACCEPTABLE, reason = "User id don't match")
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "User id don't match")
 public class UserIdDontMatchException extends RuntimeException {
 }
