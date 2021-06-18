e01fc92b71eed3f5331a9b986a99cf66ec082900
Kevin Chilito <kevin.chilito@icloud.com>
2021-06-17 19:23:16 -0500

    Change http status and message response

--- src/main/java/wolox/training/exceptions/BookIdMismatchException.java
+++ src/main/java/wolox/training/exceptions/BookIdMismatchException.java
@@ -3,9 +3,9 @@ package wolox.training.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BookIdMismatchException extends RuntimeException{
     public BookIdMismatchException() {
-        super("The id was not obtained");
+        super("Book id don't match");
     }
 }
